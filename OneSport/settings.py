a7b8463a12704145e35f80ba60d6a6c855f41005
Michael Blandón <je.sua25@hotmail.com>
2023-11-08 18:19:35 -0500

    Se reemplaza la base de datos sqlite por mysql, se configura allouth para iniciar con google

--- OneSport/settings.py
+++ OneSport/settings.py
@@ -105,8 +105,12 @@ WSGI_APPLICATION = 'OneSport.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': env('NAME'),
+        'HOST':env('HOST'),
+        'USER':env('USER'),
+        'PASSWORD':env('PASSWORD'),
+        'PORT':env('PORT'),
     }
 }
 
