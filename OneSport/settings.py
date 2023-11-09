baf69b019aff08da0a3c91724c707846c2760240
Michael Blandón <je.sua25@hotmail.com>
2023-11-09 00:26:12 -0500

    Se configura el estilo de los post

--- OneSport/settings.py
+++ OneSport/settings.py
@@ -8,7 +8,8 @@ from django.contrib.messages import constants as msj
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+LOGIN_REDIRECT_URL = "home"
+LOGOUT_REDIRECT_URL = "home"
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
